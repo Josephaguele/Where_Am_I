9fd97282e6a7e1ea99795127d3005c14b869fdc7
Joseph <osekuemen@gmail.com>
2021-03-08 21:30:14 +0100

    At this stage, the Where Am I project is enhanced to update your current location by listening for location changes with a 5-second interval.

--- app/src/main/java/com/example/whereami/WhereAmIActivity.java
+++ app/src/main/java/com/example/whereami/WhereAmIActivity.java
@@ -2,23 +2,40 @@ package com.example.whereami;
 /*This app features a new Activity that finds the device's last known location using the Fused Location
 * Provider from the Google Play services Location Services Library*/
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.IntentSender;
 import android.location.Location;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.CommonStatusCodes;
+import com.google.android.gms.common.api.ResolvableApiException;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
+import com.google.android.gms.location.LocationSettingsStates;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
+import com.google.android.gms.location.SettingsClient;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+
+import java.util.Collections;
 
 import static android.Manifest.permission.ACCESS_COARSE_LOCATION;
 import static android.Manifest.permission.ACCESS_FINE_LOCATION;
@@ -77,6 +94,8 @@ public class WhereAmIActivity extends AppCompatActivity
     //method getLastLocation to call when permission is granted or rejected:
 
     private static final int LOCATION_PERMISSION_REQUEST = 1;
+    public static final String TAG = "WhereAmIActivity";
+    private static final int REQUEST_CHECK_SETTINGS = 2;
 
     @Override
     protected void onStart()
@@ -96,7 +115,67 @@ public class WhereAmIActivity extends AppCompatActivity
             ActivityCompat.requestPermissions(this, new String[]{ACCESS_FINE_LOCATION},
                     LOCATION_PERMISSION_REQUEST);
         }
+
+        // Check of the location settings are compatible with our location
+        //Request.
+
+        /*Here, we update the onStart method to compare the system location settings with the requirements
+         * of our Location Request. If the settings are compatible, or if they can't be resolved, call the
+         * requestLocationUpdates method. If they do not meet our requirements, but can be resolved through
+         * user action, display a dialog asking the users to change their settings accordingly.*/
+
+        // Check of the location settings are compatible with our Location request.
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
+                .addAllLocationRequests(Collections.singleton(mLocationRequest));
+        SettingsClient client = LocationServices.getSettingsClient(this);
+
+
+        Task<LocationSettingsResponse> task = client.checkLocationSettings(builder.build());
+        task.addOnSuccessListener(this, new OnSuccessListener<LocationSettingsResponse>() {
+            @Override
+            public void onSuccess(LocationSettingsResponse locationSettingsResponse) {
+                // Location settings satisfy the requirements of the Location
+                // Request.
+                // Request location updates.
+                requestLocationUpdates();
+            }
+        });
+
+        task.addOnFailureListener(this, new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                // Extract the status code for the failure from within the exception.
+                int statusCode = ((ApiException)e).getStatusCode();
+                switch (statusCode)
+                {
+
+                    case CommonStatusCodes
+                            .RESOLUTION_REQUIRED:
+                    try{
+                        // Display a user dialog to resolve the location settings issue.
+                        ResolvableApiException resolvable = (ResolvableApiException) e;
+                        resolvable.startResolutionForResult(WhereAmIActivity.this,
+                                REQUEST_CHECK_SETTINGS);
+                    }catch(IntentSender.SendIntentException sendEx)
+                    {
+                        Log.e(TAG,"Location Settings resolution failed.", sendEx);
+                    }break;
+
+                    case LocationSettingsStatusCodes
+                            .SETTINGS_CHANGE_UNAVAILABLE:
+                        // Location settings issues can't be resolved by user.
+                        // Request location updates anyway.
+                        Log.d(TAG, "Location Settings can't be resolved.");
+                    requestLocationUpdates();
+                    break;
+                }
+            }
+        });
+
+
     }
+
+
 
     @Override
     public void onRequestPermissionsResult(int requestCode,
@@ -149,5 +228,53 @@ public class WhereAmIActivity extends AppCompatActivity
             latLongString = "Lat:" + lat + "\nLong:" + lng;
         }
         mTextView.setText(latLongString);
+    }
+
+
+    // this method initiates a request to receive Location updates using the Location Request
+    // defined object defined in onCreate method and the Location Callback defined
+    private void requestLocationUpdates()
+    {
+        if (ActivityCompat.checkSelfPermission(this,ACCESS_FINE_LOCATION)==PERMISSION_GRANTED
+            || ActivityCompat.checkSelfPermission(this,ACCESS_COARSE_LOCATION)==PERMISSION_GRANTED)
+        {
+            FusedLocationProviderClient fusedLocationClient =
+                    LocationServices.getFusedLocationProviderClient(this);
+
+            fusedLocationClient.requestLocationUpdates(mLocationRequest, mLocationCallback, null);
+        }
+    }
+
+    // Override the onActivityResult handler to listen for a return from the dialog potentially
+    // displayed. IF the user accepts the requested changes, request location updates. If they are
+    // rejected, check to see if any location services are available - and request updates if so:
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data)
+    {
+        final LocationSettingsStates states = LocationSettingsStates.fromIntent(data);
+
+        if (requestCode == REQUEST_CHECK_SETTINGS)
+        {
+            switch (resultCode)
+            {
+                case Activity.RESULT_OK:
+                    // Requested changes made, request location updates.
+                    requestLocationUpdates();
+                    break;
+                case Activity.RESULT_CANCELED:
+                    // Requested changes were NOT made.
+                    Log.d(TAG,"Requested settings changes declined by user.");
+                    // Check if any location services are available, and if so
+                    // request location updates.
+                    if(states.isLocationUsable())
+                        requestLocationUpdates();
+                    else
+                        Log.d(TAG,"No location services available");
+                    break;
+                default: break;
+            }
+        }
+
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
